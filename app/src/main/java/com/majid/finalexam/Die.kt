1949f34f7024957cbe84d556616472d72028d1b2
majidpawar9 <majidpawar9@gmail.com>
2023-08-10 23:51:50 -0400

    made Spinner, Roll button and roll twice option

---
+++ app/src/main/java/com/majid/finalexam/Die.kt
@@ -0,0 +1,73 @@
+package com.majid.finalexam
+
+import kotlin.random.Random
+enum class Color {
+    RED,
+    WHITE,
+    BLACK,
+    Orange
+}
+
+enum class Sides(val value: Int) {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    ELEVEN(11),
+    TWELVE(12),
+    THIRTEEN(13),
+    FOURTEEN(14),
+    FIFTEEN(15),
+    SIXTEEN(16),
+    SEVENTEEN(17),
+    EIGHTEEN(18),
+    NINETEEN(19),
+    TWENTY(20)
+}
+// Primary constructor is in the class signature
+class Die(private val color: Color, private val numSides: Sides) {
+    var sideUp: Int = 1
+        private set
+
+    // Called each time we initialize a new Die object.
+    init {
+        sideUp = Random.nextInt(1, numSides.value + 1)
+        println("Creating a ${color.name.lowercase()} d${numSides.value}")
+    }
+
+    // Two Secondary constructors that call the primary constructor
+    constructor() : this(Color.WHITE, Sides.SIX)
+    constructor(numSides: Sides) : this(Color.WHITE, numSides)
+
+    // Roll function which returns a random value
+    fun roll(): String{
+        sideUp = Random.nextInt(1, numSides.value + 1)
+        return "Rolling the ${color.name.lowercase()} d${numSides.value}..."
+    }
+    // Getter for highest side
+    fun getHighestSide(): Int{
+        return numSides.value
+    }
+
+    // Setter method to set sideUp
+    fun setSide(num: Int){
+        if(num < numSides.value){
+            println("Setting the d${numSides.value} to ${num}")
+            sideUp = num
+        }
+        else{
+            println("The Side is out of range")
+        }
+    }
+
+    // Prints die color and highest side with its current sideUp
+    override fun toString(): String {
+        return "A ${color.name.lowercase()} die with ${numSides.value} faces and currently showing a $sideUp."
+    }
+}
--- app/src/main/java/com/majid/finalexam/MainActivity.kt
+++ app/src/main/java/com/majid/finalexam/MainActivity.kt
@@ -2,10 +2,49 @@ package com.majid.finalexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.majid.finalexam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val diceOptions = arrayOf("D4", "D6", "D8", "D10", "D12", "D20")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var convertFromString:String = ""
+
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, diceOptions)
+        binding.spinnerDice.adapter = arrayAdapter
+        binding.spinnerDice.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // When user selects a unit a toast message will be shown
+                Toast.makeText(
+                    applicationContext,
+                    "Selected Unit is " + diceOptions[position],
+                    Toast.LENGTH_SHORT
+                ).show()
+                convertFromString = diceOptions[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(
+                    applicationContext,
+                    "Nothing Selected! Please select a unit",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     }
 }
