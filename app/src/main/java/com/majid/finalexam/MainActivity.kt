0b187c1355db67fcd913062e1fa2bd35aaecf4ec
majidpawar9 <majidpawar9@gmail.com>
2023-08-11 00:49:43 -0400

    Implemented Roll functionality and changed background

--- app/src/main/java/com/majid/finalexam/MainActivity.kt
+++ app/src/main/java/com/majid/finalexam/MainActivity.kt
@@ -10,16 +10,17 @@ import com.majid.finalexam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val diceOptions = arrayOf("D4", "D6", "D8", "D10", "D12", "D20")
+    val diceOptions = arrayOf("4", "6", "8", "10", "12", "20")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var convertFromString:String = ""
-
-        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, diceOptions)
+        var convertFromString: String = ""
+        var sideSelected = 0
+        val arrayAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, diceOptions)
         binding.spinnerDice.adapter = arrayAdapter
         binding.spinnerDice.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -31,10 +32,11 @@ class MainActivity : AppCompatActivity() {
                 // When user selects a unit a toast message will be shown
                 Toast.makeText(
                     applicationContext,
-                    "Selected Unit is " + diceOptions[position],
+                    "Selected Die is " + diceOptions[position],
                     Toast.LENGTH_SHORT
                 ).show()
                 convertFromString = diceOptions[position]
+                sideSelected = diceOptions[position].toInt()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -46,5 +48,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        fun rollDice(side: Int,rolls: Int) :String  {
+            var results = mutableListOf<Int>()
+            val die = Die(side)
+            val roll1 = die.roll()
+            val roll2 = die.roll()
+            results.add(roll1)
+            results.add(roll2)
+
+            if (rolls == 1) {
+                return "${results[0]}"
+            } else {
+               return "${results[0]} and ${results[1]}"
+            }
+
+        }
+        binding.rollTwiceButton.setOnClickListener {
+            val resulttext = rollDice(sideSelected,2)
+            binding.resultText.text = resulttext
+        }
+        binding.rollOnceButton.setOnClickListener {
+            val resulttext = rollDice(sideSelected,1)
+            binding.resultText.text = resulttext
+        }
     }
 }
